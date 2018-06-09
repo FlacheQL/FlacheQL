567135d808593282d166afa9f579a1d125dfaf2a
Will Howard <Will@Wills-MacBook-Pro.local>
2018-06-08 21:45:59 -0700

    Implements graphiql and schema in index.js, no modularity yet

--- github_graphql/src/index.js
+++ github_graphql/src/index.js
@@ -1,52 +1,79 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
-import express from 'express';
-import bodyParser from 'b'
+const express = require('express');
+const bodyParser = require('body-parser');
+const githubSchema = require('../schema/schema.graphql');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
 
-
+const PORT = 4000;
 const app = express();
+//was having trouble doing this in a modular fashion...how to import and all that from another file?
+const schema = buildSchema( `
+type Query {
+  info: String!
+  repository(owner: String!, name: String!): Repository
+  search(query: String!, type: String!): SearchResultItemConnection
 
-const typeDefs = `
-  type Query {
-    info: String!
-    repository(owner: String!, name: String!): 
-      Repository {
-        createdAt
-        issues(last: Int, states: String!) {
-          edges {
-            node {
-              title
-              url
-            }
-          }
-        }
-      }
-  } `
+}
+
+type Repository {
+  collaborators(first: Int!): RepositoryCollaboratorConnection
+  #createdAt: DateTime!
+  #updatedAt: DateTime!
+  databaseId: Int
+  description: String
+  name: String!
+  stargazers: StargazerConnection!
+}
+
+type StargazerConnection {
+  totalCount: Int!
+}
+
+type RepositoryCollaboratorConnection {
+  nodes: [User]
+}
+
+type User {
+  id: ID!
+  name: String!
+  repositories(first: Int): RepositoryConnection
+}
+
+type RepositoryConnection {
+  totalCount: Int!
+  nodes: [Repository]
+}
+
+#type SearchType {
+   #repository: REPOSITORY
+   #user: USER
+   #}
+
+type SearchResultItemConnection {
+  repositoryCount: Int!
+  userCount: Int!
+}
+`
+);
 
 
-  /*type Query {
-      
-  }
+/*
+endpoint: 'https://api.github.com/graphql',
+token: '5941a911a1abcbd19ca0f71ecf22b91adfa285ba'
 */
-  const resolvers = {
-    info: () => 'this is a test of the github graphql api',
-    repository: (root, args, context, info) => {
-      return context.
-    }
-    
 
-  }
 
-  const server = new GraphQLServer({
-      typeDefs: './src/schema.graphql',
-      resolvers,
-      context: req => ({
-          ...req,
-          db: new Prisma({
-            typeDefs: 'src/generated/prisma.graphql',
-            endpoint: 'https://api.github.com/graphql',
-            token: '5941a911a1abcbd19ca0f71ecf22b91adfa285ba',
-            debug: true
-          }),
-      }),
-  })
+// change to express 
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}))
+
+// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema, graphiql: true }));
+// app.get('/', bodyParser.json(), graphqlExpress({ schema: schema, graphiql: true }));
+// app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphiql' }));
+
+app.listen(PORT, () => {console.log('listening on port ', PORT)});
--- github_graphql/src/resolvers/Query.js
+++ github_graphql/src/resolvers/Query.js
@@ -2,19 +2,22 @@
 //import other dependencies (if any?)
 
 // declare variables based on jquery selector 
-
-query {
-  info: String!
-  repository(owner: String!, name: String!): 
-    Repository {
-      createdAt
-      issues(last: Int, states: String!) {
-        edges {
-          node {
-            title
-            url
-            }
+const resolvers = {
+  // context parameter in query parameter holds important contextual info like currently logged in user or access to db
+  Query: {
+    info: () => console.log('this is a test!'),
+    repository(owner: String!, name: String!):
+      Repository {
+        createdAt
+        issues(last: Int, states: String!) {
+          edges {
+            node {
+              title
+              url
+              }
+          }
         }
       }
-    }
+    
+    },
 }
