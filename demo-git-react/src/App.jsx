bca80fd3e843f36d76fdb37b770624f0b0fedfe8
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-06-09 17:58:11 -0700

    implements fetch by input field, some styling

--- demo-git-react/src/App.jsx
+++ demo-git-react/src/App.jsx
@@ -9,47 +9,62 @@ class App extends Component {
     this.state = {
       gitBoxes: []
     };
-    this.getBooksByAuthor = this.getBooksByAuthor.bind(this)
+    this.getBooksByAuthor = this.getBooksByAuthor.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
-  getBooksByAuthor() {
-    fetch('https://api.github.com/graphql', {
-      method: 'POST',
-      headers: { "Content-Type": "application/graphql", "Authorization": "token d5db50499aa5e2c144546249bff744d6b99cf87d" },
-      body: JSON.stringify({ query: `{
-        search(query: "apollo language:JavaScript stars:>100", type: REPOSITORY, first: 10) {
-          repositoryCount
-          edges {
-            node {
-              ... on Repository {
-                name
-                descriptionHTML
-                stargazers {
-                  totalCount
-                }
-                forks {
-                  totalCount
-                }
-                updatedAt
-              }
-            }
-          }
-        }
-      }`})
-    })
-    .then(res => res.json())
-    .then(res => {
-      console.log(res.data.search.edges)
-      const newBoxes = res.data.search.edges.map((repo, index) => {
-        return <GitBox key={index} name={repo.node.name} stars={repo.node.stargazers.totalCount} forks={repo.node.forks.totalCount}/>
-      });
-      console.log(newBoxes);
-      this.setState({gitBoxes: newBoxes});
-    })
-  }
   
   componentDidMount() {
     this.getBooksByAuthor();
+  }
+  
+  getBooksByAuthor(terms = 'graphql', language = 'Javascript', stars, num = 10) {
+    if (!stars || stars === 0) stars = 10;
+    console.log('fetching results... terms: ', terms, ', language: ', language, ', stars: ', stars);
+    if (isNaN(stars)) return window.alert('# of stars must be a number!');
+    fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: { "Content-Type": "application/graphql", "Authorization": "token d5db50499aa5e2c144546249bff744d6b99cf87d" },
+      body: JSON.stringify({
+        query: `{
+          search(query: "${terms} language:${language} stars:>${stars}", type: REPOSITORY, first: ${num}) {
+            repositoryCount
+            edges {
+              node {
+                ... on Repository {
+                  name
+                  descriptionHTML
+                  stargazers {
+                    totalCount
+                  }
+                  forks {
+                    totalCount
+                  }
+                  updatedAt
+                }
+              }
+            }
+          }
+        }`,
+      }),
+    })
+      .then(res => res.json())
+      .then((res) => {
+        console.log(res.data.search.edges)
+        const newBoxes = res.data.search.edges.map((repo, index) => {
+          return <GitBox key={`b${index}`} name={repo.node.name} stars={repo.node.stargazers.totalCount} forks={repo.node.forks.totalCount}/>
+        });
+        this.setState({ gitBoxes: newBoxes });
+      });
+  }
+  
+  handleSubmit() {
+    this.getBooksByAuthor(
+      document.getElementById('searchText').value,
+      document.getElementById('searchLang').value,
+      Number(document.getElementById('searchStars').value),
+      document.getElementById('searchNum').value,
+    );
   }
 
   render() {
@@ -57,17 +72,21 @@ class App extends Component {
       <div className="main-container">
         <h3>Search Repositories</h3>
         <div className="searchBoxes">
-          <label>Language: <input type="text" className="text"/></label>
+          <label>Search Terms: <input id="searchText" type="text" className="text"/></label>
         </div>
         <div className="searchBoxes">
-          <label># of ✡: <input type="text" className="text"/></label>
+          <label>Language: <input id="searchLang" type="text" className="text"/></label>
         </div>
         <div className="searchBoxes">
-          <label>Search Terms: <input type="text" className="text"/></label>
+          <label># of ✡: <input id="searchStars" type="text" className="text"/></label>
         </div>
-        <input type="button" value="Search" onClick={this.getBooksByAuthor}/>
-        {this.state.gitBoxes}
-        <GitBox/>
+        <div className="searchBoxes">
+          <label># to fetch: <input id="searchNum" type="text" className="text"/></label>
+        </div>
+        <input type="button" value="Search" onClick={this.handleSubmit} />
+        <div className="result-list">
+          {this.state.gitBoxes}
+        </div>
       </div>
     )
   }
