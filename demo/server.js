175aff1af4fe1f29a40e5db19859eb979dd29341
William Adamowicz <william.adamowicz@gmail.com>
2018-06-20 14:37:25 -0700

    add port

--- demo/server.js
+++ demo/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = 8000;
+
 app.use(express.static(path.join(__dirname, './dist')));
 
 app.get('/', (req, res) => res.sendFile('./dist/index.html'));
 
-app.listen(4001, () => console.log('Listening on 4001...'));
+app.listen(port, () => console.log(`Listening on ${port}...`));
