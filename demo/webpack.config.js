f852c4a33d5982f57439076a15a33b71a958345b
Aidan Noble-Goodman <anoblegoodman@Aidans-MacBook-Pro.local>
2019-06-05 12:30:14 -0700

    reconfigured sass to compile

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.(s*)css$/, 
+        use: ["style-loader", "css-loader","sass-loader"]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
